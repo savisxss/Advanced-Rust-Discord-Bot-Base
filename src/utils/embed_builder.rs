6e3f5a788b8159a1a608d9d6076aa1a4a3fe4d83
savis <106487343+savisxss@users.noreply.github.com>
2024-09-29 04:20:38 +0200

    Enhance EmbedBuilder with localization support and new features

--- src/utils/embed_builder.rs
+++ src/utils/embed_builder.rs
@@ -1,26 +1,53 @@
 use serenity::builder::CreateEmbed;
 use serenity::utils::Colour;
+use crate::lang::Lang;
 
-pub struct EmbedBuilder;
+pub struct EmbedBuilder<'a> {
+    lang: &'a Lang,
+}
 
-impl EmbedBuilder {
-    pub fn build_simple(title: &str, description: &str, color: Colour) -> CreateEmbed {
+impl<'a> EmbedBuilder<'a> {
+    pub fn new(lang: &'a Lang) -> Self {
+        Self { lang }
+    }
+
+    pub fn build_simple(&self, title_key: &str, description_key: &str, color: Colour) -> CreateEmbed {
         let mut embed = CreateEmbed::default();
-        embed.title(title)
-            .description(description)
+        embed.title(self.lang.get(title_key))
+            .description(self.lang.get(description_key))
             .color(color);
         embed
     }
 
-    pub fn build_error(title: &str, description: &str) -> CreateEmbed {
-        Self::build_simple(title, description, Colour::RED)
+    pub fn build_error(&self, title_key: &str, description_key: &str) -> CreateEmbed {
+        self.build_simple(title_key, description_key, Colour::RED)
     }
 
-    pub fn build_success(title: &str, description: &str) -> CreateEmbed {
-        Self::build_simple(title, description, Colour::GREEN)
+    pub fn build_success(&self, title_key: &str, description_key: &str) -> CreateEmbed {
+        self.build_simple(title_key, description_key, Colour::GREEN)
     }
 
-    pub fn build_info(title: &str, description: &str) -> CreateEmbed {
-        Self::build_simple(title, description, Colour::BLUE)
+    pub fn build_info(&self, title_key: &str, description_key: &str) -> CreateEmbed {
+        self.build_simple(title_key, description_key, Colour::BLUE)
+    }
+
+    pub fn build_custom(&self, f: impl FnOnce(&mut CreateEmbed) -> &mut CreateEmbed) -> CreateEmbed {
+        let mut embed = CreateEmbed::default();
+        f(&mut embed);
+        embed
+    }
+
+    pub fn add_field(&self, embed: &mut CreateEmbed, name_key: &str, value_key: &str, inline: bool) -> &mut CreateEmbed {
+        embed.field(self.lang.get(name_key), self.lang.get(value_key), inline)
+    }
+
+    pub fn set_footer(&self, embed: &mut CreateEmbed, text_key: &str, icon_url: Option<&str>) -> &mut CreateEmbed {
+        embed.footer(|f| {
+            f.text(self.lang.get(text_key));
+            if let Some(url) = icon_url {
+                f.icon_url(url);
+            }
+            f
+        })
     }
 }
