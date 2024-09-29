d326d97bf7acc6ca57f53fe505e05721f66757cb
savis <106487343+savisxss@users.noreply.github.com>
2024-09-29 16:13:20 +0200

    Enhance main.rs with advanced initialization and background tasks

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use dotenv::dotenv;
 use serenity::prelude::*;
 use std::env;
 use std::sync::Arc;
+use tokio::sync::broadcast;
 
 mod bot;
 mod commands;
@@ -20,14 +21,15 @@ use utils::task_manager::TaskManager;
 use utils::rate_limiter::RateLimiter;
 use utils::guild_data::GuildData;
 use lang::Lang;
+use utils::event_bus::EventBus;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     logger::init();
 
-    let config = Config::load()?;
-    let database = Database::new(&config.database.url).await?;
+    let config = Arc::new(Config::load()?);
+    let database = Arc::new(Database::new(&config.database.url).await?);
     database.run_migrations().await?;
     
     let metrics = Arc::new(Metrics::new());
@@ -35,7 +37,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let task_manager = Arc::new(TaskManager::new());
     let rate_limiter = Arc::new(RateLimiter::new());
     let guild_data = Arc::new(GuildData::new());
-    let lang = Lang::load("en")?;
+    let lang = Arc::new(Lang::load("en")?);
+
+    let (event_sender, _) = broadcast::channel(100);
+    let event_bus = Arc::new(EventBus::new(event_sender));
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let intents = GatewayIntents::GUILD_MESSAGES
@@ -45,17 +50,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler::new(
-            config.clone(),
-            database.clone(),
-            metrics.clone(),
-            cache.clone(),
-            task_manager.clone(),
-            rate_limiter.clone(),
-            guild_data.clone(),
-            lang,
+            Arc::clone(&config),
+            Arc::clone(&database),
+            Arc::clone(&metrics),
+            Arc::clone(&cache),
+            Arc::clone(&task_manager),
+            Arc::clone(&rate_limiter),
+            Arc::clone(&guild_data),
+            Arc::clone(&lang),
+            Arc::clone(&event_bus),
         ))
         .await
         .expect("Err creating client");
+
+    task_manager.spawn("metrics_reporter", {
+        let metrics = Arc::clone(&metrics);
+        async move {
+            loop {
+                tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+                metrics.report().await;
+            }
+        }
+    }).await?;
+
+    task_manager.spawn("cache_cleaner", {
+        let cache = Arc::clone(&cache);
+        async move {
+            loop {
+                tokio::time::sleep(std::time::Duration::from_secs(300)).await;
+                cache.cleanup().await;
+            }
+        }
+    }).await?;
 
     log::info!("Starting bot...");
     if let Err(why) = client.start().await {
