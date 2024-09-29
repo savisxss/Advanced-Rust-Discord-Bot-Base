be2f8fe3246a091b7af5f61b4563f42c001c543c
savis <106487343+savisxss@users.noreply.github.com>
2024-09-29 03:50:13 +0200

    Update ping command with localization and latency calculation

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,6 +1,9 @@
 use serenity::builder::CreateApplicationCommand;
-use serenity::model::application::interaction::ApplicationCommandInteraction;
+use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
+use serenity::prelude::*;
 use crate::commands::Command;
+use crate::bot::error::BotResult;
+use crate::lang::Lang;
 
 pub struct Ping;
 
@@ -9,13 +12,18 @@ impl Command for Ping {
         "ping".to_string()
     }
 
-    fn register(&self, command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-        command
-            .name("ping")
-            .description("A simple ping command")
+    fn description(&self) -> String {
+        "A simple ping command".to_string()
     }
 
-    fn run(&self, _command: &ApplicationCommandInteraction) -> String {
-        "Pong!".to_string()
+    fn register(&self, command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+        command
+            .name(self.name())
+            .description(self.description())
+    }
+
+    fn run(&self, ctx: &Context, _command: &ApplicationCommandInteraction, lang: &Lang) -> BotResult<String> {
+        let latency = ctx.cache.current_user().unwrap().id.created_at().timestamp_millis() as u64;
+        Ok(lang.get("commands.ping_response").replace("{latency}", &latency.to_string()))
     }
 }
