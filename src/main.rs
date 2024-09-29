aa4a78f9a806093a7ed2d5c5a558be10e5f86f2f
savis <106487343+savisxss@users.noreply.github.com>
2024-09-29 03:19:11 +0200

    Integrate new systems into main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use dotenv::dotenv;
 use serenity::prelude::*;
 use std::env;
 use std::sync::Arc;
-use serenity::model::gateway::Activity;
 
 mod bot;
 mod commands;
@@ -14,37 +13,46 @@ mod utils;
 use bot::handler::Handler;
 use config::Config;
 use database::Database;
+use utils::logger;
+use utils::metrics::Metrics;
+use utils::cache::Cache;
+use utils::task_manager::TaskManager;
+use utils::rate_limiter::RateLimiter;
+use utils::guild_data::GuildData;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+    logger::init();
 
-    env_logger::init();
-
-    let config = Config::load().expect("Failed to load configuration");
-
-    let database = Database::new(&config.database_url).await?;
+    let config = Config::load()?;
+    let database = Database::new(&config.database.url).await?;
+    let metrics = Arc::new(Metrics::new());
+    let cache = Arc::new(Cache::new(std::time::Duration::from_secs(300)));
+    let task_manager = Arc::new(TaskManager::new());
+    let rate_limiter = Arc::new(RateLimiter::new());
+    let guild_data = Arc::new(GuildData::new());
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT
-        | GatewayIntents::GUILD_MEMBERS;
+        | GatewayIntents::GUILDS;
 
     let mut client = Client::builder(&token, intents)
-        .event_handler(Handler::new(config.clone(), database.clone()))
+        .event_handler(Handler::new(
+            config.clone(),
+            database.clone(),
+            metrics.clone(),
+            cache.clone(),
+            task_manager.clone(),
+            rate_limiter.clone(),
+            guild_data.clone(),
+        ))
         .await
         .expect("Err creating client");
 
-    client.cache_and_http.set_activity(Activity::listening(&config.bot.status));
-
-    {
-        let mut data = client.data.write().await;
-        data.insert::<Config>(Arc::new(config));
-        data.insert::<Database>(database);
-    }
-
+    log::info!("Starting bot...");
     if let Err(why) = client.start().await {
         log::error!("Client error: {:?}", why);
     }
