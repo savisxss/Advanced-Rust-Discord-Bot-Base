d5aecf81707b679e759691664294ac577647caba
savis <106487343+savisxss@users.noreply.github.com>
2024-09-29 03:58:28 +0200

    Update error_handler with localization support

--- src/utils/error_handler.rs
+++ src/utils/error_handler.rs
@@ -1,18 +1,20 @@
 use crate::bot::error::BotError;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
+use crate::lang::Lang;
 
-pub async fn handle_error(ctx: &Context, msg: &Message, error: BotError) {
+pub async fn handle_error(ctx: &Context, msg: &Message, error: BotError, lang: &Lang) {
     let error_message = match error {
-        BotError::Serenity(why) => format!("Discord API Error: {:?}", why),
-        BotError::Database(why) => format!("Database Error: {:?}", why),
-        BotError::UnknownCommand(cmd) => format!("Unknown command: {}", cmd),
-        BotError::Config(why) => format!("Configuration Error: {}", why),
-        BotError::Internal(why) => format!("Internal Error: {}", why),
+        BotError::Serenity(why) => format!("{}: {:?}", lang.get("errors.discord_api"), why),
+        BotError::Database(why) => format!("{}: {:?}", lang.get("errors.database"), why),
+        BotError::UnknownCommand(cmd) => format!("{}: {}", lang.get("errors.unknown_command"), cmd),
+        BotError::Config(why) => format!("{}: {}", lang.get("errors.configuration"), why),
+        BotError::Internal(why) => format!("{}: {}", lang.get("errors.internal"), why),
+        BotError::RateLimit(why) => format!("{}: {}", lang.get("errors.rate_limit"), why),
     };
 
     if let Err(why) = msg.channel_id.say(&ctx.http, &error_message).await {
-        println!("Error sending error message: {:?}", why);
+        log::error!("Error sending error message: {:?}", why);
     }
 
     log::error!("{}", error_message);
