73c519a52bde546590b7400b1c34d050eb7c54da
savis <106487343+savisxss@users.noreply.github.com>
2024-09-29 03:05:34 +0200

    Extend command system with subcommands and command groups

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,46 +1,58 @@
 use serenity::builder::CreateApplicationCommand;
-use serenity::model::application::interaction::ApplicationCommandInteraction;
-
-pub mod ping;
-pub mod help;
+use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
+use crate::utils::error::BotResult;
 
 pub trait Command {
     fn name(&self) -> String;
+    fn description(&self) -> String;
     fn register(&self, command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand;
-    fn run(&self, command: &ApplicationCommandInteraction) -> String;
+    fn run(&self, ctx: &Context, command: &ApplicationCommandInteraction) -> BotResult<String>;
+}
+
+pub trait CommandGroup {
+    fn name(&self) -> String;
+    fn description(&self) -> String;
+    fn commands(&self) -> Vec<Box<dyn Command>>;
 }
 
 pub struct CommandHandler {
-    commands: Vec<Box<dyn Command>>,
+    groups: Vec<Box<dyn CommandGroup>>,
 }
 
 impl CommandHandler {
     pub fn new() -> Self {
-        Self {
-            commands: vec![
-                Box::new(ping::Ping),
-                Box::new(help::Help),
-            ],
-        }
+        Self { groups: Vec::new() }
     }
 
-    pub fn get_commands(&self) -> &Vec<Box<dyn Command>> {
-        &self.commands
+    pub fn add_group(&mut self, group: Box<dyn CommandGroup>) {
+        self.groups.push(group);
     }
 
-    pub fn find_command(&self, name: &str) -> Option<&Box<dyn Command>> {
-        self.commands.iter().find(|cmd| cmd.name() == name)
-    }
-
-    pub fn register_commands(&self, ctx: &Context, guild_id: GuildId) -> Result<(), SerenityError> {
-        let commands = guild_id.set_application_commands(&ctx.http, |commands| {
-            for command in &self.commands {
-                commands.create_application_command(|cmd| command.register(cmd));
+    pub fn register_commands(&self, ctx: &Context, guild_id: GuildId) -> BotResult<()> {
+        guild_id.set_application_commands(&ctx.http, |commands| {
+            for group in &self.groups {
+                for command in group.commands() {
+                    commands.create_application_command(|cmd| command.register(cmd));
+                }
             }
             commands
         })?;
 
-        println!("Registered the following commands: {:?}", commands);
         Ok(())
+    }
+
+    pub async fn handle_command(&self, ctx: &Context, command: &ApplicationCommandInteraction) -> BotResult<()> {
+        for group in &self.groups {
+            for cmd in group.commands() {
+                if cmd.name() == command.data.name {
+                    let response = cmd.run(ctx, command)?;
+                    command.create_interaction_response(&ctx.http, |r| {
+                        r.interaction_response_data(|d| d.content(response))
+                    }).await?;
+                    return Ok(());
+                }
+            }
+        }
+        Err(BotError::Command(format!("Unknown command: {}", command.data.name)))
     }
 }
