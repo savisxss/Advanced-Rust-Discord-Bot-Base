facd362b1fb431e22d8f96994ea58924f21cd961
savis <106487343+savisxss@users.noreply.github.com>
2024-09-29 03:49:26 +0200

    Refactor commands system and add CommandHandler

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,58 +1,54 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
-use crate::utils::error::BotResult;
+use serenity::prelude::*;
+use crate::bot::error::BotResult;
+use crate::lang::Lang;
+
+pub mod ping;
+pub mod help;
 
 pub trait Command {
     fn name(&self) -> String;
     fn description(&self) -> String;
     fn register(&self, command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand;
-    fn run(&self, ctx: &Context, command: &ApplicationCommandInteraction) -> BotResult<String>;
-}
-
-pub trait CommandGroup {
-    fn name(&self) -> String;
-    fn description(&self) -> String;
-    fn commands(&self) -> Vec<Box<dyn Command>>;
+    fn run(&self, ctx: &Context, command: &ApplicationCommandInteraction, lang: &Lang) -> BotResult<String>;
 }
 
 pub struct CommandHandler {
-    groups: Vec<Box<dyn CommandGroup>>,
+    commands: Vec<Box<dyn Command>>,
 }
 
 impl CommandHandler {
     pub fn new() -> Self {
-        Self { groups: Vec::new() }
+        let mut handler = Self { commands: Vec::new() };
+        handler.register_commands();
+        handler
     }
 
-    pub fn add_group(&mut self, group: Box<dyn CommandGroup>) {
-        self.groups.push(group);
+    fn register_commands(&mut self) {
+        self.commands.push(Box::new(ping::Ping));
+        self.commands.push(Box::new(help::Help));
     }
 
-    pub fn register_commands(&self, ctx: &Context, guild_id: GuildId) -> BotResult<()> {
-        guild_id.set_application_commands(&ctx.http, |commands| {
-            for group in &self.groups {
-                for command in group.commands() {
-                    commands.create_application_command(|cmd| command.register(cmd));
-                }
-            }
-            commands
-        })?;
-
-        Ok(())
+    pub fn get_commands(&self) -> &[Box<dyn Command>] {
+        &self.commands
     }
 
-    pub async fn handle_command(&self, ctx: &Context, command: &ApplicationCommandInteraction) -> BotResult<()> {
-        for group in &self.groups {
-            for cmd in group.commands() {
-                if cmd.name() == command.data.name {
-                    let response = cmd.run(ctx, command)?;
-                    command.create_interaction_response(&ctx.http, |r| {
-                        r.interaction_response_data(|d| d.content(response))
-                    }).await?;
-                    return Ok(());
-                }
+    pub async fn handle_command(&self, ctx: &Context, command: &ApplicationCommandInteraction, lang: &Lang) -> BotResult<String> {
+        for cmd in &self.commands {
+            if cmd.name() == command.data.name {
+                return cmd.run(ctx, command, lang);
             }
         }
-        Err(BotError::Command(format!("Unknown command: {}", command.data.name)))
+        Err(crate::bot::error::BotError::UnknownCommand(command.data.name.clone()))
+    }
+
+    pub fn register_application_commands(&self, commands: &mut CreateApplicationCommands) -> &mut CreateApplicationCommands {
+        for command in &self.commands {
+            commands.create_application_command(|create_command| {
+                command.register(create_command)
+            });
+        }
+        commands
     }
 }
